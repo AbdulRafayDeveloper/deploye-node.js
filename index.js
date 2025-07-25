3badd91b5a464706553286c660a31a8ec5af9455
Abdul Rafay <amirrafay135@gmail.com>
2025-07-25 20:36:51 +0000

    ci cd - 6

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const PORT = 5000;
 
 app.get('/api/get', (req, res) => {
-    res.send('Ci CD - Deployed on AWS Successfully!');
+    res.send('Ci CD - Deployed on AWS Successfully 1');
 });
 
-app.get('/api/get2', (req, res) => {
-    res.send({ messg: 'Ci CD - New Deployed on AWS Successfully!' });
+app.get('/api/test-api', (req, res) => {
+    res.send({ messg: 'Ci CD - New Deployed on AWS Successfully 2' });
 });
 
-app.get('/get', (req, res) => {
-    res.send({ messg: 'Ci CD - New Deployed on AWS Successfully!' });
+app.get('/api/run-new-test', (req, res) => {
+    res.send({ messg: 'Ci CD - New Deployed on AWS Successfully 3' });
 });
 
 app.listen(PORT, () => {
