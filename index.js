fbece824fee3c7d973e9193ca7bfe6a75673910e
Abdul Rafay <amirrafay135@gmail.com>
2025-07-25 20:16:10 +0000

    ci cd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
+app.get('/api/get', (req, res) => {
     res.send('Deployed on AWS Successfully!');
 });
 
